8fa353c2b4ae86d0574bb1f52ae9cf72d66fb57f
kshyun28 <jasper.d.gabriel@gmail.com>
2023-05-20 15:59:32 +0800

    feat: add login endpoint

--- src/controllers/v1/users/login/login.controller.ts
+++ src/controllers/v1/users/login/login.controller.ts
@@ -1,15 +1,40 @@
 import { Request, Response } from 'express';
 
-import { logger } from '@utils';
+import { UnauthorizedError } from '@errors';
+import { User } from '@models';
+import {
+  comparePassword,
+  errorResponse,
+  formatResponse,
+  generateJWT,
+  logger,
+} from '@utils';
 
-export const login = async (
-  _req: Request,
-  res: Response,
-): Promise<Response> => {
+export const login = async (req: Request, res: Response): Promise<Response> => {
   try {
-    return res.status(200).json(null);
-  } catch (exception: unknown) {
-    logger.error(exception);
-    return res.status(500).json(null);
+    const { username, password } = req.body;
+
+    const user = await User.findOne({
+      email: username,
+    });
+    if (!user) {
+      throw new UnauthorizedError('User does not exist');
+    }
+    if (!user.active) {
+      throw new UnauthorizedError('User is not active');
+    }
+
+    const validPassword = await comparePassword(password, user.password);
+    logger.info(validPassword);
+    if (!validPassword) {
+      throw new UnauthorizedError('Failed to login');
+    }
+
+    const token = generateJWT(username);
+    return res
+      .status(200)
+      .json(formatResponse('Successfully logged in', { token }));
+  } catch (error: unknown) {
+    return await errorResponse(res, error, 'Failed to login');
   }
 };
